f82aaad36e0e409f25ccda7bb84c5f8c50de0625
Punkrig <igorbrandaao@gmail.com>
2024-11-06 17:13:32 -0300

    Desenvolvimento: Rotas                        - Criação de rotas do projetos

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import AboutUs from "./pages/AboutUs"
-import Footer from "./pages/Footer"
-import Hero from "./pages/Hero"
-import Pricing from "./pages/Pricing"
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import Footer from "./pages/Footer";
+import Hero from "./pages/Hero";
+import Pricing from "./pages/Pricing";
+import Settings from "./pages/Settings";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
-    <main>
-      <Hero />
-      <section>
-        <AboutUs id="about-us"/>
-      </section>
-      <section>
-        <Pricing id="pricing"/>
-      </section>
-      <section>
-        <Footer />
-      </section>
-    </main>
-  )
+    <Router>
+      <Routes>
+        {/* Página Principal */}
+        <Route 
+          path="/" 
+          element={
+            <main>
+              <Hero />
+              <section>
+                <AboutUs id="about-us" />
+              </section>
+              <section>
+                <Pricing id="pricing" />
+              </section>
+              <section>
+                <Footer />
+              </section>
+            </main>
+          } 
+        />
+
+        {/* Outras rotas */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
