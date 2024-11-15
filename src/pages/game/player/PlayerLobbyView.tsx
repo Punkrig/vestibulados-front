ef22040ade387dd792f07f05b8d12ad65139b713
Punkrig <igorbrandaao@gmail.com>
2024-11-15 17:29:28 -0300

    Desenvolvimento: player lobby view            - Atualizando o Lobby

--- src/pages/game/player/PlayerLobbyView.tsx
+++ src/pages/game/player/PlayerLobbyView.tsx
@@ -4,10 +4,18 @@ const PlayerLobbyView = () => {
   return(
     <>
       <Menu />
-      <div className="w-screen h-screen flex justify-center items-center bg-white">
-        <h1 className="text-black font-bold text-9xl">
-          Esperando o Host começar o jogo!
-        </h1>
+      <div className="flex items-center justify-center w-full h-full">
+        <div className="flex justify-center items-center space-x-1 text-sm text-gray-700">
+          
+              <svg fill='none' className="w-6 h-6 animate-spin" viewBox="0 0 32 32" xmlns='http://www.w3.org/2000/svg'>
+                <path clip-rule='evenodd'
+                  d='M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z'
+                  fill='currentColor' fill-rule='evenodd' />
+              </svg>
+
+          
+          <div>Esperando o Host começar o jogo ...</div>
+        </div>
       </div>
     </>
   )
