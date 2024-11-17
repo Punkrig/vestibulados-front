f18970cb63fa8c45586140bdad6a39117fd7305f
Thiago <thiago2b9s@gmail.com>
2024-11-17 05:04:47 -0300

    constants update 2

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,33 +8,3 @@ export const materias =[
     {value:"estatistica", index:7},
     {value:"probabilidade", index:8},
 ];
-export const users =[
-    {name:"Jaozinho"},
-    {name:"Gabrielzinho"},
-    {name:"Antônio"},
-    {name:"Samuel"},
-]
-export const perguntas = [
-    {
-        enunciado: "A revolução francesa foi o marco do fim do absolutismo na europa, e acabou influenciando diversas revoluções pelo continente europeu.Sabendo disso, qual é a capital da França?",
-        index: 1,
-        alternativas: [
-            { opcao: "A", texto: "Berlim" },
-            { opcao: "B", texto: "Madri" },
-            { opcao: "C", texto: "Paris" },
-            { opcao: "D", texto: "Lisboa" }
-        ],
-        correta: "C"
-    },
-    {
-        enunciado: "Qual é o maior planeta do sistema solar?",
-        index: 2,
-        alternativas: [
-            { opcao: "A", texto: "Terra" },
-            { opcao: "B", texto: "Marte" },
-            { opcao: "C", texto: "Júpiter" },
-            { opcao: "D", texto: "Saturno" }
-        ],
-        correta: "C"
-    }
-];
