7c06b9ceb46681816b229de38302b64e13b6653d
Thiago <thiago2b9s@gmail.com>
2024-11-17 05:04:07 -0300

    constants update

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,11 @@
 export const materias =[
-    {value:"matemática",index:1},
-    {value:"física", index:2},
-    {value:"química",index:3},
+    {value:"matematica",index:1},
+    {value:"fisica", index:2},
+    {value:"quimica",index:3},
     {value:"ingles",index:4},
     {value:"literatura", index:5},
     {value:"artes", index:6},
-    {value:"estatística", index:7},
+    {value:"estatistica", index:7},
     {value:"probabilidade", index:8},
 ];
 export const users =[
